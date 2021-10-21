24ceeaade4fae562f471b0a198bd6658674b1864
StanMan14 <79800515+StanMan14@users.noreply.github.com>
2021-10-21 15:52:38 +0200

    update

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ window.onclick = function(event) {
     }
   }
 }
+
+// Option 2 - jQuery Smooth Scrolling
+// $('.navbar a').on('click', function (e) {
+//   if (this.hash !== '') {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body')
+//       .animate({
+//         scrollTop: $(hash).offset().top
+//       },800);
+//   }
+// });
+
+
+ // Option 3 - Smooth Scroll - https://github.com/cferdinandi/smooth-scroll
+//  const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+// 	speed: 500
+// });
+
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
